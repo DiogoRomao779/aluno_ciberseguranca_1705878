f5c36f583e9b849aebe44a0604c0b5b3603d825e
DiogoRomao779 <dr3amyghost@hotmail.com>
2022-12-14 14:24:22 +0000

    lasties

--- T_Practico/client.py
+++ T_Practico/client.py
@@ -2,7 +2,7 @@ import xmlrpc.client
 
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
 print("This program with implementation of RPC will present the first 100 multiples of an int.")
-x = int(input("enter the int that you want: "))
+x = int(input("Enter the int that you want: "))
 
 print(s.hundred(x))
 
