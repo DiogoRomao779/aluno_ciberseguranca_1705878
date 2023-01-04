377741bc532a32b236ec3d0518012243d24e0892
DiogoRomao779 <dr3amyghost@hotmail.com>
2023-01-04 14:57:06 +0000

    lasties

--- T_Practico/SimpleXMLRPCServer.py
+++ T_Practico/SimpleXMLRPCServer.py
@@ -2,30 +2,32 @@ from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
 
-# Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
 
-# Create server
+# Criamos o servidor
 with SimpleXMLRPCServer(('localhost', 8000),
                         requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
 
-
+# Definimos a função
     def hundred_mults(x):
-
+            #iniciamos o i a 1 para nao termos multiplos por 0
             i = 1
+            #iniciamos a variavel que sera apresentada ao cliente
             texto = ''
+            #ciclo while que ira calcular então os multiplos
             while i < 101:
                 r = 0
                 r = x * i
                 texto += str(r) + "\n"
                 i += 1
+            #returnamos entao ao cliente o resultado que foi guardado na variavel texto
             return texto
 
-
+    #registo da função criada
     server.register_function(hundred_mults, 'hundred')
 
-    # Run the server's main loop
+    #manutenção do servidor online durante tempo indefinido
     server.serve_forever()
--- T_Practico/client.py
+++ T_Practico/client.py
@@ -1,14 +1,19 @@
 import xmlrpc.client
 
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
+#apresentação inicial
 print("This program with implementation of RPC will present the first 100 multiples of an int.")
+#indicamos o X como uma string com valor 0
 x = '0'
+#condicionamos a leitura do valor inserido pelo utilizador pedindo
+#ao utilizador o tipo de valor que o programa aceita
 while x == '0' or x.isnumeric() == False:
     x = (input("Enter the int that you want, that is not 0: "))
+    #Fazemos a verificação do valor inserido pelo utilizador
     if x == '0' or '0' == 0 or x.isnumeric() == False:
         print("Wrong value, please try again!")
     else:
-        # importante fazer cast para int
+        #alteramos de string para int para efectuar pedido ao servidor
         print(s.hundred(int(x)))
         break
 
